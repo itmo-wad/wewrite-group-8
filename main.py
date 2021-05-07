5d5d39e1cba5bacc46ae2529da4f85c93be629cc
Hamid Turky <haidturkey@gmail.com>
2021-05-07 18:13:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -97,4 +97,5 @@ if __name__ == '__main__':
     from controller.favorite import *
     app.register_blueprint(favorite)
 
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=
+           False)
